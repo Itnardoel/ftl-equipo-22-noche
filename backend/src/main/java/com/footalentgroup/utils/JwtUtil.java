652367eab09501b2f1ecb01b76a0e9c7b6add2c6
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-04-17 17:36:41 -0300

    Agregar @Component a JwtUtil para permitir su inyeccion

--- backend/src/main/java/com/footalentgroup/utils/JwtUtil.java
+++ backend/src/main/java/com/footalentgroup/utils/JwtUtil.java
@@ -6,11 +6,13 @@ import org.springframework.security.core.Authentication;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
     @Value("${jwt.secret}")
     private String jwt_secret;
