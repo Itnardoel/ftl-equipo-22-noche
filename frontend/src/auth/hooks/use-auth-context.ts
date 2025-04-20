19888c36829c6433d4a19c1a2c4eee184726fb85
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-20 02:02:32 -0300

    feat: add useAuthContext hook

---
+++ frontend/src/auth/hooks/use-auth-context.ts
@@ -0,0 +1,11 @@
+import { use } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+export const useAuthContext = () => {
+  const context = use(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuthContext must be used within an AuthProvider.");
+  }
+  return context;
+};
