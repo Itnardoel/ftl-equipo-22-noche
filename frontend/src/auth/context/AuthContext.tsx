d1ab08fc50b19b5834b6bcd0a3b5fa66c1233112
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-20 02:04:04 -0300

    feat: add initial AuthContext setup with provider

---
+++ frontend/src/auth/context/AuthContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect, useMemo, ReactNode } from "react";
+import { User } from "../types";
+
+interface AuthContextType {
+  user: User | null;
+  login: (userData: User) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (userData: User) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) setUser(JSON.parse(storedUser) as User);
+  }, []);
+
+  const value = useMemo(() => ({ user, login, logout }), [user]);
+
+  return <AuthContext value={value}>{children}</AuthContext>;
+};
+
+export { AuthContext };
