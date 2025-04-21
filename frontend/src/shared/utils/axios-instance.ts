d48a3c4e739ba02e96f5c1152677f65ae01cb81e
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-21 01:28:06 -0300

    chore: add headers to Axios instance for API requests

--- frontend/src/shared/utils/axios-instance.ts
+++ frontend/src/shared/utils/axios-instance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL as string, //TODO --> tipar los .env dentro del archivo vite-env.ts
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  // withCredentials: true,
 });
