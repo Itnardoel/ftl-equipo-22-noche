f4088b8b58d29d973104d1d856e74afda09c1032
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-21 01:21:18 -0300

    docs: add TODO comments for future improvements

--- frontend/src/auth/api/index.ts
+++ frontend/src/auth/api/index.ts
@@ -1,6 +1,6 @@
 import { instance } from "@/shared/utils/axios-instance";
 import { LoginPayload, RegisterPayload, User } from "@/auth/types";
 
-export const login = (data: LoginPayload) => instance.post<User>("/auth/login", data); //TODO --> confirmar el endpoint
+export const login = (data: LoginPayload) => instance.post<User | null>("/auth/login", data); //TODO --> chequear el tipo de la respuesta <User | null>?
 
-export const register = (data: RegisterPayload) => instance.post<User>("/auth/register", data); //TODO --> confirmar el endpoint
+export const register = (data: RegisterPayload) => instance.post<User>("/auth/register", data); //TODO --> chequear el tipo de la respuesta
